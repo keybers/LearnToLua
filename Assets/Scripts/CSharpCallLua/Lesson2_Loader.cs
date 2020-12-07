23a557e21f2c3590fad58d0dd336732ba60942b4
Keyber <keyberboy@qq.com>
2020-12-07 19:43:36 +0800

    通过重写LuaFileUtils实现自定义重定向加载Lua脚本

---
+++ Assets/Scripts/CSharpCallLua/Lesson2_Loader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+using UnityEngine;
+
+public class Lesson2_Loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //之所以自定义解析类 new 了就可以用 因为父类是单例模式
+        LuaCustomLoader luaCustomLoader = new LuaCustomLoader();
+        //学习通过toLua的解析器 自定义解析路径
+
+        //声明Lua解析器 执行Lua代码
+        LuaState luaState = new LuaState();
+        luaState.Start();
+
+        //第一种 使用LuaState中的方法 AddSearchPath
+        //luaState.AddSearchPath(Application.dataPath + "/Lua/CSharpCallLua");
+        //luaState.Require("Lesson2_Loader");
+
+        //第二种 如果该文件 只属于Lua文件夹下 那么可以直接加父目录下
+        luaState.Require("CSharpCallLua/Lesson2_Loader");
+
+        //测试 新建文件夹
+        //luaState.AddSearchPath(Application.dataPath + "/MyLua");
+        //luaState.Require("My");
+
+        //主要学习目标 学会通过toLua的解析器 自定义解析方式
+        //要实现自定义解析方式 新建一个类 继承 LuaFileUtils 然后重写加载函数 就达到了自定义的目的
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CSharpCallLua/LuaCustomLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+using UnityEngine;
+
+public class LuaCustomLoader : LuaFileUtils
+{
+    //要注意这里面要加入后缀，unity识别不了lua文件
+    public override byte[] ReadFile(string fileName)
+    {
+        //如果想重新定义 解析lua的方式 那么只需要在该函数中去写逻辑
+        Debug.Log("自定义解析方式");
+
+        //如果没有lua后缀 加上最好 不管从Resources或者AB包中加载都不支持.lua后缀 所以toLua加上bytes后缀
+        //我们自己可以加上.txt后缀
+        if(!fileName.EndsWith(".lua"))
+        {
+            fileName += ".lua";
+        }
+        byte[] buffer = null;
+
+        //进行热更行的lua代码 肯定是我们自己写的上层lua逻辑
+        //第二种从AB包中加载文件
+
+        //CSharpCallLua/Lesson2_Loader这样的名字 但是在AB包中我们需要文件名 所以需要拆分一下
+        string[] strs = fileName.Split('/');
+        TextAsset luaCode = ABManager.GetInstance().LoadRes<TextAsset>("lua",strs[strs.Length - 1]);
+        if(luaCode != null)
+        {
+            buffer = luaCode.bytes;
+            Resources.UnloadAsset(luaCode);
+        }
+        //tolua的自带逻辑和自带类 我们不需要去热更新 直接从resources中加载
+
+        if(buffer == null)
+        {
+            //第一种从Resources
+            string path = "Lua/" + fileName;
+            TextAsset text = Resources.Load<TextAsset>(path);
+            if(text != null)
+            {
+                buffer = text.bytes;
+                //卸载使用后文本资源
+                Resources.UnloadAsset(text);
+            }    
+        }
+
+
+        //返回bytes资源
+        return buffer;
+    }
+}
